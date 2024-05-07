0b4d9438e1d3fa8480e4edac270e5cd8a391be19
bitsoko services <bitsokokenya@gmail.com>
2024-05-07 12:43:41 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1924,7 +1924,7 @@ function startMap() {
 
 function onWebsiteLoaded(event) {
 	// load audio
-	audioManager.load('ocean', null, '/audio/ocean.mp3')
+	audioManager.load('ocean', null, '/audio/trap.mp3')
 	audioManager.load('propeller', null, '/audio/propeller.mp3')
 
 	audioManager.load('coin-1', 'coin', '/audio/coin-1.mp3')
