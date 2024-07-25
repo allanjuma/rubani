b80255ff77fa12fa0c7acd6262dd84d4d42f6caa
bitsoko services <bitsokokenya@gmail.com>
2024-07-25 11:28:21 +0000

    Update game.js

--- game.js
+++ game.js
@@ -2063,55 +2063,6 @@ tapp.onEvent('invoiceClosed', function(e){
 
 
 
-    
-    
-  const walletsList = await tonConnectUI.getWallets();
-  console.log(walletsList);
-  
-  const currentWallet = tonConnectUI.wallet;
-const currentWalletInfo = tonConnectUI.walletInfo;
-const currentAccount = tonConnectUI.account;
-const currentIsConnectedStatus = tonConnectUI.connected;
-
-console.log(currentWallet, currentWalletInfo, currentAccount, currentIsConnectedStatus);
-
-
-
-
-
-
-
-
-
-
-	
-	const transaction = {
-    validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
-    messages: [
-        {
-            address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
-            amount: "20000000",
-         // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
-        },
-        {
-            address: "EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn",
-            amount: "60000000",
-         // payload: "base64bocblahblahblah==" // just for instance. Replace with your transaction payload or remove
-        }
-    ]
-}
-
-try {
-    const result = await tonConnectUI.sendTransaction(transaction);
-
-    // you can use signed boc to find the transaction 
-    //const someTxData = await myAppExplorerService.getTransaction(result.boc);
-    
-    
-    alert('Transaction was sent successfully');
-} catch (e) {
-    console.error(e);
-}
 
 	ui = new UI(startMap)
 	loadingProgressManager
