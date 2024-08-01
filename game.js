d65de58af4600353f1a581504732a5637f917e1d
bitsoko services <bitsokokenya@gmail.com>
2024-08-01 14:27:08 +0000

    Update game.js

--- game.js
+++ game.js
@@ -2120,10 +2120,9 @@ function startMap() {
 // create an async function to request a wake lock
 try {
   wakeLock = await navigator.wakeLock.request("screen");
-  statusElem.textContent = "Wake Lock is active!";
 } catch (err) {
   // The Wake Lock request has failed - usually system related, such as battery.
-  statusElem.textContent = `${err.name}, ${err.message}`;
+  console.log("wake lock inactive! "+err);
 }
 
 }
