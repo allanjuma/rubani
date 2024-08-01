8404f3c488e06d58a9a98468207789be4d9e5ab4
bitsoko services <bitsokokenya@gmail.com>
2024-08-01 09:10:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -375,7 +375,7 @@ function doMint(address, amount){
           address: "kQAGL6r5BaATeS5r0NuvMgzC5H2cdrzwcMIcZieMJW7hszbB",
           amount: ton.toNano(0.04).toString(),
           stateInit: undefined,
-          payload: mintBody(Address.parse(address), amount, ton.toNano(0.02), 0)
+          payload: mintBody(ton.Address.parse(address), amount, ton.toNano(0.02), 0)
             .toBoc()
             .toString("base64"),
         },
