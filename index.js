558e28e630b58e380c8d2b577bb8b64f8f956af2
bitsoko services <bitsokokenya@gmail.com>
2024-08-05 15:06:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const proxyTon = ston.pTON.v2.create(
         
 
         rubsParentWallet = "0QA_FaPINkfLXs_KY0O9Sw_GkAiY8QthpAqyYIzjhW03a4cg";     // unique
-        rubsContractAddress = "kQBOOOsLX6P_jwrtXenbnpdXIpljuR2DMnKXtxrdJY2XnFLv";  //unique
+        rubsContractAddress = "kQC6yFic6HKWd4CA8qb64OyeN1mrKXuq_lMju7y2qqPHL0rJ";  //unique
         
    
 const mnemonic = "duty mistake ready edge wool toss know reject extend state judge grit empower rifle phrase raise spring easily census picture pen sibling traffic absent";
