a082869cc983395f1be4408e35714162f2377f0c
bitsoko services <bitsokokenya@gmail.com>
2025-03-18 09:53:25 +0000

    Update main.js

--- chains/hbar/main.js
+++ chains/hbar/main.js
@@ -3,60 +3,188 @@ require("dotenv").config();
 
 
 // import the 'Client' module from the Hedera JS SDK
-const { Client, CryptoTransferTransaction } = require("@hashgraph/sdk");
-
+const {
+    AccountCreateTransaction,
+    AccountDeleteTransaction,
+    Wallet,
+    LocalProvider,
+    PrivateKey,
+    Hbar,
+    TokenAssociateTransaction,
+    TokenCreateTransaction,
+    TokenDeleteTransaction,
+    TokenGrantKycTransaction,
+    TransferTransaction,
+    TokenWipeTransaction,
+    TransactionId,
+} from "@hashgraph/sdk";
 
 
 /* create a new asynchronous function */
 
+
 async function main() {
+    if (
+        process.env.OPERATOR_ID == null ||
+        process.env.OPERATOR_KEY == null ||
+        process.env.HEDERA_NETWORK == null
+    ) {
+        throw new Error(
+            "Environment variables OPERATOR_ID, HEDERA_NETWORK, and OPERATOR_KEY are required.",
+        );
+    }
 
+    const provider = new LocalProvider();
 
+    const wallet = new Wallet(
+        process.env.OPERATOR_ID,
+        process.env.OPERATOR_KEY,
+        provider,
+    );
 
-/* grab our testnet credentials from our .env file */
+    const newKey = PrivateKey.generate();
 
-const operatorAccount = process.env.ACCOUNT_ID;
+    console.log(`private key = ${newKey.toString()}`);
+    console.log(`public key = ${newKey.publicKey.toString()}`);
 
-const operatorPrivateKey = process.env.PRIVATE_KEY;
+    try {
+        let transaction = await new AccountCreateTransaction()
+            .setKeyWithoutAlias(newKey.publicKey)
+            .setInitialBalance(new Hbar(2))
+            .freezeWithSigner(wallet);
+        transaction = await transaction.signWithSigner(wallet);
+        let resp = await transaction.executeWithSigner(wallet);
 
+        const transactionReceipt = await resp.getReceiptWithSigner(wallet);
+        const newAccountId = transactionReceipt.accountId;
 
+        console.log(`account id = ${newAccountId.toString()}`);
 
-console.log(operatorPrivateKey, operatorAccount);
+        let tokenCreateTransaction = await new TokenCreateTransaction()
+            .setNodeAccountIds([resp.nodeId])
+            .setTokenName("ffff")
+            .setTokenSymbol("F")
+            .setDecimals(3)
+            .setInitialSupply(100)
+            .setTreasuryAccountId(wallet.getAccountId())
+            .setAdminKey(wallet.getAccountKey())
+            .setFreezeKey(wallet.getAccountKey())
+            .setWipeKey(wallet.getAccountKey())
+            .setKycKey(wallet.getAccountKey())
+            .setSupplyKey(wallet.getAccountKey())
+            .setFreezeDefault(false)
+            .freezeWithSigner(wallet);
+        tokenCreateTransaction =
+            await tokenCreateTransaction.signWithSigner(wallet);
+        resp = await tokenCreateTransaction.executeWithSigner(wallet);
 
-/* configure our testnet client */
+        const tokenId = (await resp.getReceiptWithSigner(wallet)).tokenId;
+        console.log(`token id = ${tokenId.toString()}`);
 
-const client = Client.forTestnet();
+        await (
+            await (
+                await (
+                    await (
+                        await new TokenAssociateTransaction()
+                            .setNodeAccountIds([resp.nodeId])
+                            .setAccountId(newAccountId)
+                            .setTokenIds([tokenId])
+                            .freezeWithSigner(wallet)
+                    ).sign(newKey)
+                ).signWithSigner(wallet)
+            ).executeWithSigner(wallet)
+        ).getReceiptWithSigner(wallet);
 
-console.log(client);
+        console.log(
+            `Associated account ${newAccountId.toString()} with token ${tokenId.toString()}`,
+        );
 
-client.setOperator(operatorAccount, operatorPrivateKey);
+        await (
+            await (
+                await (
+                    await new TokenGrantKycTransaction()
+                        .setNodeAccountIds([resp.nodeId])
+                        .setAccountId(newAccountId)
+                        .setTokenId(tokenId)
+                        .freezeWithSigner(wallet)
+                ).signWithSigner(wallet)
+            ).executeWithSigner(wallet)
+        ).getReceiptWithSigner(wallet);
 
+        console.log(
+            `Granted KYC for account ${newAccountId.toString()} on token ${tokenId.toString()}`,
+        );
 
+        await (
+            await (
+                await (
+                    await new TransferTransaction()
+                        .setNodeAccountIds([resp.nodeId])
+                        .addTokenTransfer(tokenId, wallet.getAccountId(), -10)
+                        .addTokenTransfer(tokenId, newAccountId, 10)
+                        .freezeWithSigner(wallet)
+                ).signWithSigner(wallet)
+            ).executeWithSigner(wallet)
+        ).getReceiptWithSigner(wallet);
 
-/* send our first hbar transfer! */
+        console.log(
+            `Sent 10 tokens from account ${wallet
+                .getAccountId()
+                .toString()} to account ${newAccountId.toString()} on token ${tokenId.toString()}`,
+        );
 
-const transactionId = await new CryptoTransferTransaction()
+        await (
+            await (
+                await (
+                    await new TokenWipeTransaction()
+                        .setNodeAccountIds([resp.nodeId])
+                        .setTokenId(tokenId)
+                        .setAccountId(newAccountId)
+                        .setAmount(10)
+                        .freezeWithSigner(wallet)
+                ).signWithSigner(wallet)
+            ).executeWithSigner(wallet)
+        ).getReceiptWithSigner(wallet);
 
-.addSender(operatorAccount, 1)
+        console.log(`Wiped balance of account ${newAccountId.toString()}`);
 
-.addRecipient("0.0.3", 1)
+        await (
+            await (
+                await (
+                    await new TokenDeleteTransaction()
+                        .setNodeAccountIds([resp.nodeId])
+                        .setTokenId(tokenId)
+                        .freezeWithSigner(wallet)
+                ).signWithSigner(wallet)
+            ).executeWithSigner(wallet)
+        ).getReceiptWithSigner(wallet);
 
-.setTransactionMemo("Hello future!")
+        console.log(`Deleted token ${tokenId.toString()}`);
 
-.execute(client);
+        await (
+            await (
+                await (
+                    await (
+                        await new AccountDeleteTransaction()
+                            .setNodeAccountIds([resp.nodeId])
+                            .setAccountId(newAccountId)
+                            .setTransferAccountId(wallet.getAccountId())
+                            .setTransactionId(
+                                TransactionId.generate(newAccountId),
+                            )
+                            .setMaxTransactionFee(new Hbar(1))
+                            .freezeWithSigner(wallet)
+                    ).sign(newKey)
+                ).signWithSigner(wallet)
+            ).executeWithSigner(wallet)
+        ).getReceiptWithSigner(wallet);
 
+        console.log(`Deleted account ${newAccountId.toString()}`);
+    } catch (error) {
+        console.error(error);
+    }
 
-
-/* get the receipt of this transfer */
-
-const receipt = await transactionId.getReceipt(client);
-
-console.log(receipt);
-
+    provider.close();
 }
 
-
-
-/* call our async function */ 
-
-main();
+void main();
