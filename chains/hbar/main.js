4e2928cf6b230b71882e655a000421a31a49392c
bitsoko services <bitsokokenya@gmail.com>
2025-03-18 10:45:39 +0000

    Update main.js

--- chains/hbar/main.js
+++ chains/hbar/main.js
@@ -53,17 +53,20 @@ async function main() {
 
         /**
          * 2. Create a custom network with GRPC web proxies
-         */
+         * 
+         * 
         const nodes = {
             "https://testnet-node02-00-grpc.hedera.com:443": new AccountId(5),
             "https://testnet-node03-00-grpc.hedera.com:443": new AccountId(6),
             "https://testnet-node04-00-grpc.hedera.com:443": new AccountId(7),
         };
 
-        /**
-         * 3. Setup the client using "Client"
-         */
         const client = Client.forNetwork(nodes);
+         * 
+         * 
+         */
+        
+      const client = Client.forTestnet();
 
         client.setOperator(operatorId, operatorKey);
         const debugLogger = new Logger(LogLevel.Debug);
