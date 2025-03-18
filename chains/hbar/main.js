dbf9ced680c49fe97df180135e65c3d43b2e8376
bitsoko services <bitsokokenya@gmail.com>
2025-03-18 09:40:25 +0000

    Update main.js

--- chains/hbar/main.js
+++ chains/hbar/main.js
@@ -7,32 +7,56 @@ require("dotenv").config();
 const { Client } = require("@hashgraph/sdk");
 
 
+/* create a new asynchronous function */
 
 async function main() {
 
 
 
-// Grab our account credentials
+/* grab our testnet credentials from our .env file */
 
 const operatorAccount = process.env.ACCOUNT_ID;
 
 const operatorPrivateKey = process.env.PRIVATE_KEY;
 
 
+
 console.log(operatorPrivateKey, operatorAccount);
 
-// Configure a testnet client with our Account ID & private key
+/* configure our testnet client */
 
 const client = Client.forTestnet();
+
+console.log(client);
 
 client.setOperator(operatorAccount, operatorPrivateKey);
 
 
-console.log(client);
-// add the rest of your code here 
 
-// ...
+/* send our first hbar transfer! */
+
+const transactionId = await new CryptoTransferTransaction()
+
+.addSender(operatorAccount, 1)
+
+.addRecipient("0.0.3", 1)
+
+.setTransactionMemo("Hello future!")
+
+.execute(client);
+
+
+
+/* get the receipt of this transfer */
+
+const receipt = await transactionId.getReceipt(client);
+
+console.log(receipt);
 
 }
+
+
+
+/* call our async function */ 
 
 main();
